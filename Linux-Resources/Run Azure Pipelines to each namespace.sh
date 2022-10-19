0611684557a6b8ecca13f2092e5d50f7c268843f
mtloaprhn <aperuchon@martellotech.com>
2022-10-19 12:33:06 +0200

    Add files

---
+++ Linux-Resources/Run Azure Pipelines to each namespace.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+tenants=`cat swo-wave2-1.txt | sed 's/\r$//' | cut -d "." -f1`
+for t in $tenants
+do
+  az pipelines run --organization https://gsxsolutions.visualstudio.com/ --project CBMT --id 257 --variables "CLIENTNAME=$t" "SOFT_DELETE_KEYVAULT=true"
+  echo "############################# updating tenant: $t #####################################"
+  sleep 9m
+done
---
+++ Microsoft-Resources/Install-L2TP-VPN.ps1
@@ -0,0 +1,4 @@
+﻿New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\PolicyAgent" -Name "AssumeUDPEncapsulationContextOnSendRule" -Value "2" -PropertyType "Dword"
+Install-Module -Name VPNCredentialsHelper -Force
+Add-VpnConnection -Name "" -ServerAddress "vpn.domain.com" -TunnelType "L2tp" -L2tpPsk "" -AuthenticationMethod MsChapv2 -RememberCredential -PassThru -Force
+Set-VpnConnectionUsernamePassword -connectionname "" -username "" -password ""
