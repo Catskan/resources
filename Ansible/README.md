24ff0910c8f8d6f52fdc2191c79efb6ed72b68dd
aperuchon <aperuchon@gsx.com>
2022-11-13 21:38:47 +0100

    New ansible

--- Ansible/README.md
+++ Ansible/README.md
@@ -7,7 +7,7 @@
 *Example :*
 `ansible-playbook -i /share/Ansible/Inventory/hosts.yaml /share/Ansible/Macbook/Remove-FondueDeDeco.yaml`
 
-ansible-playbook -i /share/Ansible/Inventory/hosts.yaml /share/Ansible/Gaming/main_playbook.yml
+ansible-playbook -i /share/Ansible/Inventory/hosts.yaml /share/Ansible/Gaming/main_playbook.yml --ask-vault-password
 ansible-playbook -i /share/Ansible/Inventory/hosts.yaml /share/Ansible/Gaming/User.yaml
 
 ansible-playbook -i /share/Ansible/Inventory/hosts.yaml /share/Ansible/Gaming/main_remove_softwares.yml
--- Microsoft-Resources/Untitled1.ps1
+++ Microsoft-Resources/Untitled1.ps1
@@ -1,15 +1,15 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url = "https://raw.githubusercontent.com/jborean93/ansible-windows/master/scripts/Install-WMF3Hotfix.ps1"
 $file = "$env:temp\Install-WMF3Hotfix.ps1"
-
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
-powershell.exe -ExecutionPolicy ByPass -File $file -
+powershell.exe -ExecutionPolicy ByPass -File $file
 
 Start-Service WinRM
 winrm quickconfig
-cmd.exe /C winrm set winrm/config/service/auth @{Basic="true"}
+winrm set winrm/config/service/auth '@{Basic="true"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+Install-Module PSCX -Force -AllowClobber
 
-Get-NetAdapter
-Get-NetConnectionProfile
-Set-NetConnectionProfile -InterfaceAlias Ethernet -NetworkCategory Private
+#Get-NetAdapter
+#Get-NetConnectionProfile
+#Set-NetConnectionProfile -InterfaceAlias Ethernet -NetworkCategory Private
