669748ec30250cd3346d2a972cfc008a99c8a3ee
Catskan <a.peruchon@icloud.com>
2022-11-14 20:08:45 +0000

    Add settings bitwarden

--- Microsoft-Resources/Powershell Scripts/Manage_JSON.ps1
+++ Microsoft-Resources/Powershell Scripts/Manage_JSON.ps1
@@ -2,18 +2,7 @@ function SettingsAppSettings.json {
 
     $FileParsed = Get-Content -Raw -Path "C:\Users\Aurel\AppData\Roaming\Bitwarden\data.json" | ConvertFrom-Json
 
+    $FileParsed.global.environmentUrls.base = "{{ bitwarden_server_url }}"
     $FileParsed.global.rememberedEmail = "{{ bitwarden_email }}"
-    $FileParsed.AppSettings.DbConfig.Database = $SQL_Database
-    $FileParsed.AppSettings.DbConfig.UseWindowsCredentials = $SQL_UseWindowsCredentials
-    $FileParsed.AppSettings.DbConfig.UserID = $SQL_Username
-    $FileParsed.AppSettings.DbConfig.Password = $SQL_Password_Encoded
-
-    $FileParsed.AppSettings.BusConfig.Hostname = $RMQ_Hostname
-    $FileParsed.AppSettings.BusConfig.VirtualHostname = $RMQ_Vhost
-    $FileParsed.AppSettings.BusConfig.ExchangeName = "robot-manager"
-    $FileParsed.AppSettings.BusConfig.Port = $RMQ_Port
-    $FileParsed.AppSettings.BusConfig.Username = $RMQ_Username
-    $FileParsed.AppSettings.BusConfig.Password = $RMQ_Password_Encoded
-    $FileParsed.AppSettings.BusConfig.UseSsl = $RMQ_SSL
-
-    $FileParsed | ConvertTo-Json | Out-File "$InstallFolderPath\GSX-Web-UI\WebAPI\appsettings.json" -Force
+    
+    $FileParsed | ConvertTo-Json | Out-File "C:\Users\Aurel\AppData\Roaming\Bitwarden\data.json" -Force
