1bc1ff044155ae42fad83bb90899e108b3a7438d
aperuchon <aperuchon@gsx.com>
2023-01-16 19:34:39 +0100

    Add if

--- Linux-Resources/Scripts/Install_github_runner.sh
+++ Linux-Resources/Scripts/Install_github_runner.sh
@@ -1,10 +1,16 @@
 runner_name=github-actions-runner
 runner_version=2.299.1
-
+cpu_model_Apple="Apple M1"
 # Create a folder
 mkdir $runner_name && cd $runner_name
 # Download the latest runner package
-curl -o $runner_name-linux-x64-$runner_version.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-$runner_version.tar.gz
+if (sysctl -n machdep.cpu.brand_string) != "Apple M1"
+then
+    curl -o $runner_name-linux-x64-$runner_version.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-$runner_version.tar.gz
+else if (sysctl -n machdep.cpu.brand_string) == "Apple M1"
+    then
+    curl -o actions-runner-osx-arm64-2.300.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.300.2/actions-runner-osx-arm64-2.300.2.tar.gz
+fi;
 # Optional: Validate the hash
 echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  $runner_name-linux-x64-$runner_version.tar.gz" | shasum -a 256 -c
 # Extract the installer
