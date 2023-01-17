58566c4ac83c23a9db26c21abb0f1d6624975672
Catskan <a.peruchon@icloud.com>
2023-01-17 18:02:21 +0100

    Case

--- Linux-Resources/Scripts/Install_github_runner.sh
+++ Linux-Resources/Scripts/Install_github_runner.sh
@@ -1,18 +1,29 @@
 runner_name=github-actions-runner
 runner_version=2.299.1
 cpu_model_Apple="Apple M1"
+kernel="$(sysctl -a | grep -E 'ostype' | awk '{print $2, $3}')"
+
+case $kernel in
+    *"Linux"*)
+        echo "It's Linux";;
+    *"Darwin"*)
+        echo "It's MacOS";;
+esac
 # Create a folder
 mkdir $runner_name && cd $runner_name
 # Download the latest runner package
-if [ "$(sysctl -a | grep -E 'ostype' | awk '{print $2, $3}')" = "Linux" ]
+if [ "$(sysctl -a | grep -E 'ostype' | awk '{print $2, $3}')" == *'Linux'* ]
 then
-    os_type=linux
-    curl -o $runner_name-$os_type-$runner_version.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-$runner_version.tar.gz
-elif [ "$(sysctl -a | grep -E 'ostype' | awk '{print $2, $3}')" = "Darwin" ]
+    echo "$(sysctl -a | grep -E 'ostype' | awk '{print $2, $3}')"
+    # os_type=linux
+    # curl -o $runner_name-$os_type-$runner_version.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-$runner_version.tar.gz
+elif [ "$(sysctl -a | grep -E 'ostype' | awk '{print $2, $3}')" == *'Darwin'* ]
 then
-    os_type=macOS
-    curl -o $runner_name-$os_type-$runner_version.tar.gz -L https://github.com/actions/runner/releases/download/v2.300.2/actions-runner-osx-arm64-$runner_version.tar.gz
+    echo "$(sysctl -a | grep -E 'ostype' | awk '{print $2, $3}')"
+    # os_type=macOS
+    # curl -o $runner_name-$os_type-$runner_version.tar.gz -L https://github.com/actions/runner/releases/download/v2.300.2/actions-runner-osx-arm64-$runner_version.tar.gz
 fi
+
 # Optional: Validate the hash
 echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  $runner_name-$os_type-$runner_version.tar.gz" | shasum -a 256 -c
 # Extract the installer
