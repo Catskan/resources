442fc6195f2dd7955d8c687779736c778d53f9ed
aperuchon <aperuchon@gsx.com>
2023-01-17 12:20:35 +0100

    Add condition to download righ os

--- Linux-Resources/Scripts/Install_github_runner.sh
+++ Linux-Resources/Scripts/Install_github_runner.sh
@@ -4,17 +4,19 @@ cpu_model_Apple="Apple M1"
 # Create a folder
 mkdir $runner_name && cd $runner_name
 # Download the latest runner package
-if (sysctl -n machdep.cpu.brand_string) != "Apple M1"
+if [ "$(sysctl -n machdep.cpu.brand_string)" != "Apple M1" ]
 then
-    curl -o $runner_name-linux-x64-$runner_version.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-$runner_version.tar.gz
-else if (sysctl -n machdep.cpu.brand_string) == "Apple M1"
-    then
-    curl -o actions-runner-osx-arm64-2.300.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.300.2/actions-runner-osx-arm64-2.300.2.tar.gz
-fi;
+    os_type=linux
+    curl -o $runner_name-$os_type-$runner_version.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-$runner_version.tar.gz
+elif [ "$(sysctl -n machdep.cpu.brand_string)" = "Apple M1" ]
+then
+    os_type=macOS
+    curl -o $runner_name-$os_type-$runner_version.tar.gz -L https://github.com/actions/runner/releases/download/v2.300.2/actions-runner-osx-arm64-$runner_version.tar.gz
+fi
 # Optional: Validate the hash
-echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  $runner_name-linux-x64-$runner_version.tar.gz" | shasum -a 256 -c
+echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  $runner_name-$os_type-$runner_version.tar.gz" | shasum -a 256 -c
 # Extract the installer
-tar xzf ./$runner_name-linux-x64-$runner_version.tar.gz
+tar xzf ./$runner_name-$os_type-$runner_version.tar.gz
 # Create the runner and start the configuration experience
 ./config.sh --url https://github.com/Catskan/resources --token $GITHUB_TOKEN
 # Last step, run it!
