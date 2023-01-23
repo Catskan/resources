70e73f280e5c4b500934b77903c3c4c2e11eae93
aperuchon <aperuchon@gsx.com>
2023-01-23 18:04:26 +0100

    Delete TF

--- Terraform/Private AKS Cluster deployment/terraform/env/example/backend.tfvars
+++
@@ -1,4 +0,0 @@
-resource_group_name="example-tfstate"
-storage_account_name="tfexample"
-container_name="tfexample"
-key="example.tfstate"
--- Terraform/Private AKS Cluster deployment/terraform/modules/sql_pool/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "sqlserver_id" {
-  description = "Id of the created SQL server"
-  value       = azurerm_mssql_server.sqlserver.id
-}
