287b2c2f935e3c4d9704894dfc26dc906618ea90
aperuchon <aperuchon@gsx.com>
2023-01-27 19:17:15 +0100

    Add comments

--- Microsoft-Resources/Azure DevOPS/Pipelines/Release/pipelines-files/linux-files/scripts/postgres/create-db-postgres.sql
+++
@@ -1,6 +0,0 @@
-CREATE ROLE #{kcDBadminUser}# PASSWORD '#{kcDBadminPwd}#' LOGIN;
-ALTER ROLE #{kcDBadminUser}# PASSWORD '#{kcDBadminPwd}#';
-CREATE DATABASE #{kcDBname}#;
-grant all privileges on database #{kcDBname}# to #{kcDBadminUser}#;
-GRANT "#{kcDBadminUser}#" TO "#{kcPostgreSqlServerAdminLogin}#";
-\q
--- Microsoft-Resources/Powershell Scripts/Prepare_Winrm_for_Ansible.ps1
+++
@@ -1,4 +0,0 @@
-#Script to set correctly local Winrm with HTTP and basic auth to manage Windows nodes by using Ansible
-
-#Enable WinRM
-winrm quickconfig
